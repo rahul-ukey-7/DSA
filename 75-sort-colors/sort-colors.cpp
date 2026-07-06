ba25a1e72805a23f57e10775512f949fdda2aa0a
rahul-ukey-7 <www.rahulukey28@gmail.com>
2026-07-06 21:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.87%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+
+        int l=0;
+        int mid=0;
+        int h=n-1;
+
+        while(mid<=h)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[l++],nums[mid++]);
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid],nums[h--]);
+            }
+        }
+        
+    }
+};
