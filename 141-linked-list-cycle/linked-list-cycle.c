7a8959645c2d0711f9cf927a8c844da55bf45a0f
rahul-ukey-7 <www.rahulukey28@gmail.com>
2026-07-07 21:15:10 +0530

    Time: 11 ms (46.72%) | Memory: 11.8 MB (98.19%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -0,0 +1,19 @@
+//Name: Rahul Ukey
+//MIS: 112515216
+bool hasCycle(struct ListNode *head) {
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL){
+
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast){
+            return true;
+        }
+    }
+
+    return false;
+}
