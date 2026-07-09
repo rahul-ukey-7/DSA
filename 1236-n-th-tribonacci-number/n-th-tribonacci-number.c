bd0e6624267f07529c68d3f835e3a29c3d689428
rahul-ukey-7 <www.rahulukey28@gmail.com>
2026-07-09 21:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (41.25%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.c
@@ -0,0 +1,24 @@
+int tribonacci(int n) {
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n==1 || n==2)
+    {
+        return 1;
+    }
+    else
+    {
+        int a=0,b=1,c=1,d;
+
+        for(int i=3;i<=n;i++)
+        {
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+
+        return c;
+    }
+}
