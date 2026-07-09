661af1a93283f53c31c48ad0c7abe613654cfb1d
rahul-ukey-7 <www.rahulukey28@gmail.com>
2026-07-09 21:00:25 +0530

    Time: 3 ms (58.35%) | Memory: 8.5 MB (67.22%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.c
@@ -0,0 +1,20 @@
+//Name: Rahul Ukey
+//MIS: 112515216
+int firstBadVersion(int n) {
+
+    int start = 1;
+    int end = n;
+
+    while (start < end){
+
+        int mid = start + (end - start) / 2;
+
+        if (isBadVersion(mid)){
+            end = mid;
+        }else{
+            start = mid + 1;
+        }
+    }
+    return start;
+}
+
